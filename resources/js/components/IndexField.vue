76edc7157d3d7e8021fb08e2b6d987087cd8217e
Ro Kleine Sonne <ro.sonne@gmail.com>
2019-10-25 14:02:00 +0200

    icon fix

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -2,28 +2,6 @@
   <div class="flex items-center">
     <button
       v-if="field.last != resourceId"
-      @click="reorderResource('bottom')"
-      class="cursor-pointer text-70 hover:text-primary mr-3"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="22"
-        width="22"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="3"
-        viewBox="0 0 24 24"
-        class="fill-transparent"
-      >
-        <circle cx="12" cy="12" r="10" />
-        <polyline points="8 12 12 16 16 12" />
-        <line x1="12" x2="12" y1="8" y2="16" />
-      </svg>
-    </button>
-
-    <button
-      v-if="field.last != resourceId"
       @click="reorderResource('down')"
       class="cursor-pointer text-70 hover:text-primary mr-3"
     >
@@ -39,8 +17,54 @@
         class="fill-transparent"
       >
         <circle cx="12" cy="12" r="10" />
+        <polyline points="8 10 12 14 16 10" />
+        <!-- <line x1="12" x2="12" y1="8" y2="16" /> -->
+      </svg>
+    </button>
+
+    <button
+      v-if="field.last != resourceId"
+      @click="reorderResource('bottom')"
+      class="cursor-pointer text-70 hover:text-primary mr-3"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="22"
+        width="22"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        class="fill-transparent"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <polyline points="8 8 12 12 16 8" />
         <polyline points="8 12 12 16 16 12" />
-        <line x1="12" x2="12" y1="8" y2="16" />
+        <!-- <line x1="12" x2="12" y1="8" y2="16" /> -->
+      </svg>
+    </button>
+
+    <button
+      v-if="field.first != resourceId"
+      @click="reorderResource('top')"
+      class="cursor-pointer text-70 hover:text-primary"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="22"
+        width="22"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        class="fill-white"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <polyline points="8 12 12 8 16 12" />
+        <polyline points="8 16 12 12 16 16" />
+        <!-- <line x1="12" x2="12" y1="16" y2="8" /> -->
       </svg>
     </button>
 
@@ -61,32 +85,9 @@
         class="fill-white"
       >
         <circle cx="12" cy="12" r="10" />
-        <polyline points="16 12 12 8 8 12" />
-        <line x1="12" x2="12" y1="16" y2="8" />
+        <polyline points="8 14 12 10 16 14" />
+        <!-- <line x1="12" x2="12" y1="16" y2="8" /> -->
       </svg>
-    </button>
-
-    <button
-      v-if="field.first != resourceId"
-      @click="reorderResource('top')"
-      class="cursor-pointer text-70 hover:text-primary"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="22"
-        width="22"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="3"
-        viewBox="0 0 24 24"
-        class="fill-white"
-      >
-        <circle cx="12" cy="12" r="10" />
-        <polyline points="16 12 12 8 8 12" />
-        <line x1="12" x2="12" y1="16" y2="8" />
-      </svg>
-      top
     </button>
   </div>
 </template>
