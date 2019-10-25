b75ece402753a73cbd67a375cf8c23aa51b07ee3
Ro Kleine Sonne <ro.sonne@gmail.com>
2019-10-25 13:47:41 +0200

    added fatter icons for top / bottom buttons

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -5,12 +5,21 @@
       @click="reorderResource('bottom')"
       class="cursor-pointer text-70 hover:text-primary mr-3"
     >
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="fill-transparent">
-                <circle cx="12" cy="12" r="10"/>
-                <polyline points="8 12 12 16 16 12"/>
-                <line x1="12" x2="12" y1="8" y2="16"/>
-      </svg>-->
-      bottom
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="22"
+        width="22"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="3"
+        viewBox="0 0 24 24"
+        class="fill-transparent"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <polyline points="8 12 12 16 16 12" />
+        <line x1="12" x2="12" y1="8" y2="16" />
+      </svg>
     </button>
 
     <button
@@ -62,11 +71,21 @@
       @click="reorderResource('top')"
       class="cursor-pointer text-70 hover:text-primary"
     >
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" height="22" width="22" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="fill-white">
-                <circle cx="12" cy="12" r="10"/>
-                <polyline points="16 12 12 8 8 12"/>
-                <line x1="12" x2="12" y1="16" y2="8"/>
-      </svg>-->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="22"
+        width="22"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="3"
+        viewBox="0 0 24 24"
+        class="fill-white"
+      >
+        <circle cx="12" cy="12" r="10" />
+        <polyline points="16 12 12 8 8 12" />
+        <line x1="12" x2="12" y1="16" y2="8" />
+      </svg>
       top
     </button>
   </div>
